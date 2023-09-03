99edc853f137d2e54ed2f1e7ddf3ae6850401fb0
MATHEUSPARREIRA <mparreira03@gmail.com>
2023-09-03 14:09:47 -0300

    novo

--- Conta.java
+++ Conta.java
@@ -3,5 +3,20 @@ public class Conta {
     String dono;//atributo
     double saldo;//atributo
     double limite;//atributo
+    double salario;
+    void saca(double quantidade){//(double quantidade) é o argumento ou parâmetro
+        /*void diz que, quando você pedir para a
+        conta sacar uma quantia, nenhuma informação
+        será enviada de volta a quem pediu.*/
+        //double novoSaldo = this.saldo - quantidade;
+        //this.saldo = novoSaldo;
+    //poderia ser "this.saldo -= quantidade;"
+        this.saldo -= quantidade;
+    }
 
+    void deposita(double quantidade){
+       this.saldo += quantidade;/*+= soma quantidade ao valor
+       antigo do saldo e guarda no próprio saldo, o valor resultante.*/
+
+    }
 }
--- Programa.java
+++ Programa.java
@@ -1,5 +1,18 @@
 public class Programa {
     public static void main(String[] args) {
-        new Conta();//instanciando a classe Conta
+        Conta minhaConta;
+        /*Através da variável minhaConta,
+        podemos acessar o objeto recém criado (Conta)
+        para alterar seu dono, seu saldo etc:*/
+        minhaConta = new Conta();//objeto Conta
+
+        minhaConta.dono = "Matheus";
+        minhaConta.saldo = 1000.0;
+
+        minhaConta.saca(200);
+        minhaConta.deposita(500);
+
+        System.out.println("Saldo atual: " + minhaConta.saldo);
+
     }
 }
